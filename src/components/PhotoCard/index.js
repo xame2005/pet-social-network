c67bf399cc80b3c4c0c8bedec284be59a37b4789
Samuel Gonzalez <xame2005@gmail.com>
2020-12-06 21:13:29 -0600

    [Created] Browser polyfills with dynamic imports

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -11,18 +11,23 @@ export const PhotoCard = ({ id, likes = 0, src }) => {
   const [show, setShow] = useState(false);
   useEffect(
     function () {
-      const observer = new window.IntersectionObserver(function (entries) {
-        {
-          const { isIntersecting } = entries[0];
-          if (isIntersecting) {
-            console.log("Sí");
-            setShow(true);
-            observer.disconnect();
+      Promise.resolve(
+        typeof window.IntersectionObserver !== "undefined"
+          ? window.IntersectionObserver
+          : import("intersection-observer")
+      ).then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          {
+            const { isIntersecting } = entries[0];
+            if (isIntersecting) {
+              console.log("Sí");
+              setShow(true);
+              observer.disconnect();
+            }
           }
-          console.log(isIntersecting);
-        }
+        });
+        observer.observe(element.current);
       });
-      observer.observe(element.current);
     },
     [element.current]
   );
